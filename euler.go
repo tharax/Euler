6f9cb61c63d7180ad6c1fd3ad129f5c094cf3c74
Peter Rosser <tharax@gmail.com>
2012-08-12 21:27:49 +1200

    changed file structure

---
+++ euler.go
@@ -0,0 +1,12 @@
+// This file creates the menu which runs all the solutions to Project Euler
+// Problems.
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is the master Euler Program.")
+}
