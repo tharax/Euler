577dea59275a4c9088689a8476fa04f20c749518
Peter Rosser <tharax@gmail.com>
2012-08-21 10:23:23 +1200

    euler1 will run independently

--- euler1.go
+++ euler1.go
@@ -1,12 +1,22 @@
-package euler
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Testing
+	fmt.Println(Problem1(0, 0) == 0)
+	fmt.Println(Problem1(0, 10) == 23)
+
+	fmt.Println(Problem1(0, 1000))
+}
 
 func Problem1(start, finish int) int {
 	var sum int
 	for ; start < finish; start++ {
-		if start%3 == 0 {
-			sum = sum + start
-		} else if start%5 == 0 {
-			sum = sum + start
+		if start%3 == 0 || start%5 == 0 {
+			sum += start
 		}
 	}
 	return sum
