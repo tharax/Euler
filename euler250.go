1b1b7eb9ecbffbdadd70fbd2267a9d564919e254
Peter Rosser <tharax@gmail.com>
2012-08-27 22:32:49 +1200

    re-working comments on algorithm

--- euler250.go
+++ euler250.go
@@ -36,15 +36,9 @@ import (
 )
 
 func main() {
-	t0 := time.Now()
-	remainders := arrayOfRemainders(250250)
-	fmt.Println(remainders)
-	t1 := time.Now()
-	fmt.Printf("creating remainders took %v to run.\n", t1.Sub(t0))
-	countForEachRemainder := countEachRemainder(remainders)
-	fmt.Println(countForEachRemainder)
-	t2 := time.Now()
-	fmt.Printf("counting each remainder took %v to run.\n", t2.Sub(t1))
+
+	Problem250(250250)
+
 	// testArray := []int{1, 2, 3, 4, 5}
 	// countOfCombinations := countCombinations(testArray)
 	// fmt.Println(countOfCombinations)
@@ -53,11 +47,29 @@ func main() {
 	// amounts := []int{10, 10, 10, 10, 10, 10}
 
 	// test := combinations(amounts, len(amounts)) //5 is the number that its meant to add to.
-	var m map[int]uint64
-	test := combinations(m, 5)
-	t3 := time.Now()
-	fmt.Printf("\nCounting each combination took %v to run.\n", t3.Sub(t2))
-	fmt.Println("test =", test)
+	// var m map[int]uint64
+	// test := combinations(m, 5)
+	// // t3 := time.Now()
+	// //fmt.Printf("\nCounting each combination took %v to run.\n", t3.Sub(t2))
+	// fmt.Println("test =", test)
+
+}
+
+func Problem250(number int) {
+
+	// Calculate the remainder for each number in the set
+	startTime := time.Now()
+	remainders := arrayOfRemainders(number)
+	// fmt.Println(remainders)
+	endTime := time.Now()
+	fmt.Printf("Creating remainder array took %v to run.\n\n", endTime.Sub(startTime))
+
+	// Count each remainder, returns a smaller set (of length 250)
+	startTime = time.Now()
+	countForEachRemainder := countEachRemainder(remainders)
+	fmt.Println(countForEachRemainder)
+	endTime = time.Now()
+	fmt.Printf("Counting each remainder took %v to run.\n\n", endTime.Sub(startTime))
 
 }
 
@@ -81,8 +93,6 @@ func main() {
 
 // 17 635 968 000 000 000 000
 
-// for array len 6
-
 // function(x) 
 // if x = 1, return the current count of x 
 // if else split into x-y and y
@@ -90,171 +100,25 @@ func main() {
 
 // 11 ways to add 6. 7 ways to add 5. 5 ways to add 4. 3 ways to add 3. definitely a prime sequence.
 // dont know how that will come in handy yet.
+// len(array) == 6. There are at least 3 algorithms that I can see, listed below.
 
-// 6 
-// 5+1			
-// 4+2
-// 4+1+1
-// 3+3			
-// 3+2+1			
-// 3+1+1+1		
-// 2+2+2		
-// 2+2+1+1
-// 2+1+1+1+1		
-// 1+1+1+1+1+1	
+// 1. Exhaust all of the combos where array[0] is highest, before reducing array[0] by 1...
+// 2. All combos of len 1, then 2, then 3...
+// 3. A wierd mix of trying to spread all numbers (I wrote this one ages ago and could be wrong about the order)
 
-// 6 
-// 5+1			
-// 4+2
-// 4+1+1
-// 3+3			
-// 3+2+1			
-// 3+1+1+1		
-// 2+2+2		
-// 2+2+1+1
-// 2+1+1+1+1		
-// 1+1+1+1+1+1	
+// 6				6				6
+// 5+1				5+1				5+1
+// 4+2				4+2				4+1+1
+// 4+1+1			3+3				3+1+1+1
+// 3+3				4+1+1			2+1+1+1+1
+// 3+2+1			3+2+1			1+1+1+1+1+1
+// 3+1+1+1			2+2+2			4+2
+// 2+2+2			3+1+1+1			3+3
+// 2+2+1+1			2+2+1+1			3+2+1
+// 2+1+1+1+1		2+1+1+1+1		2+2+2
+// 1+1+1+1+1+1		1+1+1+1+1+1		2+2+1+1
 
-// 6
-// 5+1
-// 4+2
-// 3+3
-// 4+1+1
-// 3+2+1
-// 2+2+2
-// 3+1+1+1
-// 2+2+1+1
-// 2+1+1+1+1
-// 1+1+1+1+1+1
-
-// 6 
-// 5+1			
-// 4+1+1
-// 3+1+1+1		
-// 2+1+1+1+1		
-// 1+1+1+1+1+1
-// 4+2
-// 3+3			
-// 3+2+1			
-// 2+2+2		
-// 2+2+1+1
-
-func combinations(m map[int]uint64, max int) uint64 {
-
-	m = make(map[int]uint64)
-	var maximum, total uint64
-	maximum = uint64(max)
-
-	fmt.Println("maximum =", maximum)
-	for i := 0; fib(i, m) < maximum; i++ {
-		m[i] = fib(i, m)
-		fmt.Println(m, m[i])
-	}
-	for i := 0; fib(i, m) < maximum; i++ {
-		if m[i]%2 == 0 {
-			fmt.Println(m[i])
-			total = total + m[i]
-		}
-	}
-	fmt.Println("total =", total)
-	return total
-}
-
-func fib(x int, m map[int]uint64) uint64 {
-
-	if x < 1 {
-		return 1
-	}
-	return m[x-1] + m[x-2]
-}
-
-/*func combinations(amounts []int, goal int) []int {
-	combo := make([]int, 0)
-	combo[0] = goal
-
-	fmt.Println("start of combinations", combo, len(combo), amounts, len(amounts))
-
-	for i := goal - 1; i >= goal/2; i-- {
-		combo[0] = i
-		fmt.Println(combo[i+1:], ".")
-		a := combo[0:]
-		b := combinations(combo, goal-1)
-		combo = append(a, b...)
-		// for j := 0; j < i; j++ {
-		// 	fmt.Print(i)
-		// 	combo[j] = i
-		// 	combo[j+1] = goal - i
-		// 	if combo[j+1] > 1{
-		// 		fmt.Println(combo[j+1:], ".")
-		// 		a := combo[0:j+1]
-		// 		b := combinations(combo[j+1:], combo[j+1])
-		// 		combo = append(a, b...)
-		// 		fmt.Println("inner loop combo =", combo)
-		// 	}
-		// 	// : len(combo)-1] = combinations(combo[1:len(combo)-1], goal-j)
-		// 	fmt.Println("middle loop combo =", combo)
-		// }
-
-	}
-	fmt.Println("returning call of combinations combo =", combo)
-
-	// for i := goal - 1; i >= goal/2; i-- {
-
-	// 	// j := 0
-	// 	for j := 0; j < i; j++ {
-	// 		fmt.Print(i)
-	// 		combo[j] = i
-	// 		combo[j+1] = goal - i
-	// 		if combo[j+1] > 1{
-	// 			fmt.Println(combo[j+1:], ".")
-	// 			a := combo[0:j+1]
-	// 			b := combinations(combo[j+1:], combo[j+1])
-	// 			combo = append(a, b...)
-	// 			fmt.Println("inner loop combo =", combo)
-	// 		}
-	// 		// : len(combo)-1] = combinations(combo[1:len(combo)-1], goal-j)
-	// 		fmt.Println("middle loop combo =", combo)
-	// 	}
-
-	// }
-	// fmt.Println("returning call of combinations combo =", combo)
-
-	return combo
-}*/
-
-/* Takes a number and returns all the prime divisors, without repeating previous numbers.
- * eg. 10 returns [2 5], 100 returns [2 5], 8 returns [2].
- */
-func nonRepeatingDivisorsOf(upper int64) []int {
-	var primeSlice = make([]int, 0)
-	for i := int64(2); i <= upper; i++ {
-		if upper%i == 0 {
-			primeSlice = append(primeSlice, int(i))
-			for upper%i == 0 {
-				upper = upper / i
-			}
-		}
-	}
-	return primeSlice
-}
-
-/* Takes a number and returns all the prime divisors, with repeating previous numbers.
- * eg. 10 returns [2 5], 100 returns [2 2 5 5], 8 returns [2 2 2].
- */
-func allDivisorsOf(upper int64) []int {
-	var primeSlice = make([]int, 0)
-	for i := int64(2); i <= upper; i++ {
-		for upper%i == 0 {
-			primeSlice = append(primeSlice, int(i))
-			upper = upper / i
-
-		}
-	}
-	return primeSlice
-}
-
-/* Returns the remainder of (x^x / 250)  
- */
+// Returns the remainder of (x^x / 250)  
 func arrayOfRemainders(x int) []int {
 	remainderSlice := make([]int, 0)
 	for i := 1; i <= x; i++ {
@@ -262,7 +126,7 @@ func arrayOfRemainders(x int) []int {
 	J:
 		for j := 1; j < i; j++ {
 			remainder = remainder * i % 250
-			//steps = append(steps, remainder)
+			// steps = append(steps, remainder)
 			for k := 0; k < len(steps); k++ {
 				if remainder == steps[k] {
 					remainder = steps[i%len(steps)]
@@ -271,10 +135,6 @@ func arrayOfRemainders(x int) []int {
 				}
 			}
 			steps = append(steps, remainder)
-			// fmt.Println("i", i, "j", j, "remainder", remainder)
-			// if remainder == remainder * i % 250 {
-			// 	break
-			// }
 		}
 		remainderSlice = append(remainderSlice, remainder)
 	}
@@ -283,24 +143,8 @@ func arrayOfRemainders(x int) []int {
 
 func countEachRemainder(array []int) []int {
 	count := make([]int, 250)
-	//fmt.Println(len(array))
 	for i := 0; i < len(array); i++ {
 		count[int(math.Abs(float64(array[i])))]++ // = count[n] + 1
 	}
 	return count
 }
-
-
-// 
-// func countCombinations(array []int) int {
-// fmt.Println(array, len(array))
-// count := array[0]
-// 
-// for i := len(array)-1; i >= 0; i-- {
-// fmt.Println("count + array[", i, "] * array[", len(array)-i, "]")
-// fmt.Println(count, "+", array[i], "*", array[len(array)-i], "=")
-// count = count + array[i]*countCombinations(array[i:len(array)-i])
-// }
-// return count
-// }
-// 
