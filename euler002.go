d243c01693fbae73460f972bccb5d18d088ce671
Peter Rosser <tharax@gmail.com>
2012-09-01 20:05:54 +1200

    Abstracted out smaller parts of the function so I can reuse them.

--- euler002.go
+++ euler002.go
@@ -2,20 +2,15 @@ package main
 
 import "fmt"
 
-var m map[int]int
-
 func main() {
 	Problem2(4000000)
 }
 
 func Problem2(maximum int) {
-	m = make(map[int]int)
 	var total int
 	fmt.Println("maximum =", maximum)
-	for i := 0; fib(i) < maximum; i++ {
-		m[i] = fib(i)
-	}
-	for i := 0; fib(i) < maximum; i++ {
+	m := createFibSequence(maximum)
+	for i := 0; fib(i, m) < maximum; i++ {
 		if m[i]%2 == 0 {
 			fmt.Println(m[i])
 			total = total + m[i]
@@ -24,7 +19,15 @@ func Problem2(maximum int) {
 	fmt.Println("total =", total)
 }
 
-func fib(x int) int {
+func createFibSequence(maximum int) map[int]int {
+	m := make(map[int]int)
+	for i := 0; fib(i, m) < maximum; i++ {
+		m[i] = fib(i, m)
+	}
+	return m
+}
+
+func fib(x int, m map[int]int) int {
 	if x < 1 {
 		return 1
 	}
